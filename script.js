d7bcac5735cdabe9bd4a687c1e984424ebdae205
Karolina Pietruszka <pietruszka.karolina94@gmail.com>
2019-08-27 19:17:11 +0200

    task finished

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
-// ----- Controls -----
-
-class Controls extends React.Component {
-    static propTypes = {
-        className: React.PropTypes.string.isRequired,
-        buttons: React.PropTypes.array.isRequired
-    }
-
-    render() {
-        return (
-            <nav className="controls">
-                {this.props.buttons.map((button, index) => {
-                        return (
-                            <a href={'#'} onClick={() => button.action()} className={this.props.className} key={index}>{button.text}</a>
-                        )
-                })}
-            </nav>
-        )
-    }
-
-};
-
-// ----- App -----
-
-class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <Stopwatch />
-            </div>
-        )
-    }
-};
-
-
-// ----- Stopwatch -----
-
 class Stopwatch extends React.Component {
     constructor() {
         super();
-
-        this.intervalId = null;
         this.state = {
-            running: false,
             times: {
                 minutes: 0,
                 seconds: 0,
                 miliseconds: 0
             }
-        }
+        };
+        this.running = false;
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.reset = this.reset.bind(this);
 
     }
 
@@ -57,32 +21,34 @@ class Stopwatch extends React.Component {
                 minutes: 0,
                 seconds: 0,
                 miliseconds: 0
-            }   
-        })
+            }
+        });
+        this.print();
     }
 
-    pad0(value) {
-            let result = value.toString();
-            return result.length < 2 ? '0' + result : result;
+    print() {
+
     }
 
     format(times) {
-        return `${this.pad0(times.minutes)}:${this.pad0(times.seconds)}:${this.pad0(Math.floor(times.miliseconds))}`;
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     }
 
     start() {
         if (!this.state.running) {
-            this.setState({
-                running: true
-            })
-            this.intervalId = setInterval(() => this.calculate(), 10);
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
         }
     }
 
-    calculate() {
-        if (!this.state.running) return;
-        const times = this.state.times;
+    step() {
+        if (!this.running) return;
+        this.calculate();
 
+    }
+
+    calculate() {
+        const times = this.state.times;
         times.miliseconds += 1;
         if (times.miliseconds >= 100) {
             times.seconds += 1;
@@ -92,38 +58,45 @@ class Stopwatch extends React.Component {
             times.minutes += 1;
             times.seconds = 0;
         }
-
-        this.setState({
-            times
-        })
+        this.setState({ times });
     }
 
     stop() {
-        this.setState({
-            running: false
-        })
-        clearInterval(this.intervalId);
-        this.intervalId = null;
+        this.running = false;
+        clearInterval(this.watch);
     }
 
     render() {
-        const formatedTime = this.format(this.state.times);
-        const buttons = [
-            {text: "Start", action: this.start.bind(this)},
-            {text: "Stop", action: this.stop.bind(this)},
-            {text: "Reset", action: this.reset.bind(this)}
-        ]
-        return (
-            <div>
-                <Controls className="buttons" buttons={buttons}/>
-                <div className="stopwatch">{formatedTime}</div>
-            </div>
-        )
-    }
 
+        return (
+            <div className="container">
+                <nav className="controls">
+                    <a className="buttonstart" href="#" onClick={this.start}>
+                        Start
+                    </a>
+                    <a className="buttonstop" href="#" onClick={this.stop}>
+                        Stop
+                    </a>
+                    <a className="buttonreset" href="#" onClick={this.reset}>
+                        Reset
+                    </a>
+                </nav>
+                <div className="stopwatch">
+                    {this.format(this.state.times)}
+                </div>
+                <div className="results"></div>
+            </div>
+        );
+    }
 }
 
-// ----- rendering -----
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
 
-const app = <App />;
-ReactDOM.render(app, document.getElementById('app'));
+const app = document.getElementById('app')
+ReactDOM.render(<Stopwatch />, app);
